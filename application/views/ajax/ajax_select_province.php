fd001d4a27641d496e6d304ef9db5d41a4f27012
chawanrat_chaiya <palm@turkenterprisesinc.com>
2016-02-11 12:44:40 +0700

    delete wasted

--- application/views/ajax/ajax_select_province.php
+++
@@ -1,5 +0,0 @@
-
-<option value="">Please select Business City or Province</option>
-<?php for($i=0;$i<count($province);$i++){  ?>
-	<option value="<?php echo $province[$i]['PROVINCE_ID'];?>"><?php echo $province[$i]['PROVINCE_NAME'];?></option>
-<?php } ?>
